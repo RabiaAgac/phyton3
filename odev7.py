f26289a955736fa04b523c716df6cde80cc1ac12
RabiaAgac <rabiaa.agac@gmail.com>
2024-12-30 21:30:18 +0300

    Update odev7.py

--- odev7.py
+++ odev7.py
@@ -7,16 +7,12 @@
         ürün güncelle    => urunGuncelle(1, "iphone 15 pro", 80000)
         ürünleri listele => urunleriGetir() 
 """
-# Module 1: Database (db)
+# Module 1: 
 urunler = []
 
-# Module 2: Methods
+# Module 2: 
 def urunEkle(urunAdi, fiyat):
-    yeniUrun = {
-        "id": len(urunler) + 1,
-        "urunAdi": urunAdi,
-        "fiyat": fiyat
-    }
+    yeniUrun = {"id": len(urunler) + 1, "urunAdi": urunAdi,"fiyat": fiyat}
     urunler.append(yeniUrun)
     return yeniUrun
 
@@ -31,14 +27,10 @@ def urunGuncelle(urunId, yeniUrunAdi, yeniFiyat):
 def urunleriGetir():
     return urunler
 
-# Module 3: Application
-# Yeni ürün ekleme
 urunEkle("iphone 15", 60000)
 
-# Ürün güncelleme
 urunGuncelle(1, "iphone 15 pro", 80000)
 
-# Ürünleri listeleme
 liste = urunleriGetir()
 for urun in liste:
     print(f"ID: {urun['id']}, Ürün: {urun['urunAdi']}, Fiyat: {urun['fiyat']}")
